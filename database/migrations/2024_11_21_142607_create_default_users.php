17d19d49f0ce555af84b450ed36f5d6f15d9a926
Lucas Zacharias <lucas@zacharias-net.de>
2026-06-15 22:07:14 +0200

    sdsdsd

--- database/migrations/2024_11_21_142607_create_default_users.php
+++ database/migrations/2024_11_21_142607_create_default_users.php
@@ -24,6 +24,22 @@ return new class extends Migration
                 'status' => true,
                 'deleted_at' => null,
             ],
+            [
+                'id' => 2,
+                'name' => 'Mark Zacharias',
+                'email' => 'mark@zacharias-net.de',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$4G/b4N2yiEP0tBx4DHBuAeDWrUPVROqpB6dn73DTzNwz6jotMgJjC',
+                'two_factor_secret' => null,
+                'two_factor_recovery_codes' => null,
+                'two_factor_confirmed_at' => null,
+                'remember_token' => null,
+                'current_team_id' => 1,
+                'profile_photo_path' => null,
+                'role' => 'admin',
+                'status' => true,
+                'deleted_at' => null,
+            ],
         ];
 
         foreach ($users as $user) {
@@ -43,6 +59,7 @@ return new class extends Migration
         DB::table('users')
             ->whereIn('email', [
                 'lucas@zacharias-net.de',
+                'mark@zacharias-net.de',
             ])
             ->delete();
     }
